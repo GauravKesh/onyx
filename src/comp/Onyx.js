68e2556ddf5a7ca3448fef996edea544c9218d1a
Gaurav Kesh Roushan <gkrcoder@gmail.com>
2023-12-02 21:31:10 +0530

    commit

--- src/comp/Onyx.js
+++ src/comp/Onyx.js
@@ -7,26 +7,92 @@ export default function Onyx() {
             <div className="main-c" >
                 <div className="part1">
                     <div className="part1-1">
-                        <div className="val"><span><img src="" alt="logo" /></span><h1>Serene</h1><span>.com</span></div>
+                        <div className="val">
+                            <div className="logo"></div>
+                            <span style={{ fontSize: "40px" }}><b>Seren</b>.com</span>
+
+                        </div>
                     </div>
-                    <div className="part1-2"></div>
-                    <div className="part1-3"></div>
-                    <div className="part1-4"></div>
-                    <div className="part1-5"></div>
-                    <div className="part1-6"></div>
+                    <div className="part1-2">
+                        <div className="d">
+                            <span></span><h1>Dashboard</h1></div>
+
+                    </div>
+                    <div className="part1-3">
+                        <div className="d">
+                            <span></span><h1>Previous Adventure</h1></div>
+
+
+                    </div>
+                    <div className="part1-4">
+                        <div className="d">
+                            <span></span><h1>Bucket List</h1></div></div>
+                    <div className="part1-5"><div className="d">
+                        <span></span><h1>Transaction</h1></div></div>
+                    <div className="part1-6"> <div className="d">
+                        <span></span><h1>Settings</h1></div>
+                    </div>
                     <div className="part1-7"></div>
-                    <div className="part1-8"></div>
+                    <div className="part1-8"> <div className="d">
+                        <span></span>Logout</div>
+                    </div>
                 </div>
 
-
+                <span></span>
                 <div className="part2">
-                <h1>Hello Harsha,</h1>
-                <span><p>Escape into the adventure that awaits</p></span>
-                
+                    <h1>Hello Harsha,</h1>
+                    <span><p>Escape into the adventure that awaits</p></span>
+                    <div className="image">
+                        <div className="p1"><img src="https://shorturl.at/jzJO0" alt="1" /></div>
+
+                        <div className="p2"><img src="https://shorturl.at/jzJO0" alt="2" /></div>
+                        <div className="p3"><img src="https://shorturl.at/jzJO0" alt="3" /></div>
+                    </div>
+                    <div className="p22">
+                        <h1>Discover Adventures</h1>
+                        <div className="p33">
+
+                            <div className="p4"><img src="" alt="4" /></div>
+                            <div className="p5"><img src="" alt="5" /></div>
+                            <div className="p6"><img src="" alt="6" /></div>
+                            <div className="p7"><img src="" alt="7" /></div></div>
+
+                    </div>
+
                 </div>
-                <div className="part3"></div>
+                <span></span>
+                <div className="part3">
+                    <div className="frm">
+                        <input type="search" name="search" id="search" placeholder='Search Adventures' />
+                    </div>
+                    <div className="call">
+                        <h1>Seren Calender</h1>
+                        <div className="calender">
+
+
+
+                        </div>
+                    </div>
+                    <div className="eve">
+                        <h1>Upcoming Adventures</h1>
+                        <div className="d1">
+                            <div className="d11"></div>
+                            <div className="d12"></div>
+
+                        </div>
+                        <div className="d2">
+                            <div className="d21"></div>
+                            <div className="d22"></div>
+                        </div>
+                        <div className="d3">
+                            <div className="d31"></div>
+                            <div className="d32"></div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
+
     )
 
 }
