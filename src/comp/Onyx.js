e7544a1a981536069ade10f84bb3ba497a37d58f
Gaurav Kesh Roushan <gkrcoder@gmail.com>
2023-12-02 19:43:49 +0530

    onyx

---
+++ src/comp/Onyx.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import '../comp/onyx.css';
+
+export default function Onyx() {
+    return (
+        <div className="main">
+            <div className="main-c" >
+                <div className="part1">
+                    <div className="part1-1">
+                        <div className="val"><span><img src="" alt="logo" /></span><h1>Serene</h1><span>.com</span></div>
+                    </div>
+                    <div className="part1-2"></div>
+                    <div className="part1-3"></div>
+                    <div className="part1-4"></div>
+                    <div className="part1-5"></div>
+                    <div className="part1-6"></div>
+                    <div className="part1-7"></div>
+                    <div className="part1-8"></div>
+                </div>
+
+
+                <div className="part2">
+                <h1>Hello Harsha,</h1>
+                <span><p>Escape into the adventure that awaits</p></span>
+                
+                </div>
+                <div className="part3"></div>
+            </div>
+        </div>
+    )
+
+}
